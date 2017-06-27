e82924520a937cc17e8be64892f772f36bab6582
Gabriel Ifeanyi <gabriel_ifeanyi@ymail.com>
2017-06-27 23:38:05 +0100

    First Commit

---
+++ app/src/main/java/gabriel1/com/funfacts/FunFactsActivity.java
@@ -0,0 +1,76 @@
+package gabriel1.com.funfacts;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class FunFactsActivity extends ActionBarActivity {
+
+    TextView factLabel;
+    Button showFactButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        //assign views from layout files to their respective string variables
+        factLabel = (TextView) findViewById(R.id.factTextView);
+        showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The button was clicked so update the fact with a new fact
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult, you will have 206.",
+                        "It takes about 8 minutes for the light from the sun to reach earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2 to 3 meters out of water.",
+                        "On average it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built."
+                };
+
+                // Randomly select a fact
+                Random random = new Random();
+                int randomNumber = random.nextInt(facts.length);
+
+                //Update the label with our dynamic fact
+                factLabel.setText(facts[randomNumber]);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
